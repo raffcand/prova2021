5f056034456557fbc01c7b68cf3ef3c95b8b1ed2
raffa <raffa@LAPTOP-NND9FO0D.mshome.net>
2021-11-09 11:36:33 +0100

    Secondo commit

--- Prova/src/main/java/it/unisa/prova/Main.java
+++ Prova/src/main/java/it/unisa/prova/Main.java
@@ -8,6 +8,7 @@ package it.unisa.prova;
 public class Main {
     public static void main(String args[]){
         System.out.println("Hello, world");
+        System.out.println("Hello, universe");
     }
     
 }
