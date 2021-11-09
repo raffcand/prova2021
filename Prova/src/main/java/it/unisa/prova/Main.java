7dd4f5860034a1f085a35ec8e46bff727174033f
raffa <raffa@LAPTOP-NND9FO0D.mshome.net>
2021-11-09 11:41:23 +0100

    Aggiunto messaggio per Zuckemberg

--- Prova/src/main/java/it/unisa/prova/Main.java
+++ Prova/src/main/java/it/unisa/prova/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String args[]){
         System.out.println("Hello, world");
         System.out.println("Hello, universe");
+        System.out.println("Hello, metaverse");
     }
     
 }
